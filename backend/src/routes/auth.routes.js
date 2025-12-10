e49c4f0d8df5e272b8f940d01ef4abeb90aaf349
lidy03 <emanuellemarinho15@gmail.com>
2025-12-10 15:17:54 -0300

    fix: Corrige rota de autênticação

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,7 +5,6 @@ const { authMiddleware } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
-
 router.get('/me', authMiddleware, getMe);
 
 module.exports = router;
